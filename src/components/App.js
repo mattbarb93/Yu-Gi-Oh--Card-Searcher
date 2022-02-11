724d3343da5a3398f16d169d9163d9e6bb096464
Matheus Barbosa <mattbarb93@gmail.com>
2022-02-10 22:54:02 -0500

    Ygoprodeck API hooked

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,16 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import ygoprodeck from "../api/ygoprodeck";
 
 class App extends React.Component {
   state = { cards: [] };
+
+  onSearchSubmit = async (term) => {
+    const response = await ygoprodeck.get("/api/v7/cardinfo.php", {
+      params: { fname: term },
+    });
+    this.setState({ cards: response.data.data });
+  };
 
   render() {
     return (
