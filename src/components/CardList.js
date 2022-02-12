d4f2a20ea3797f970fd388bdba324cab4fde87e0
Matheus Barbosa <mattbarb93@gmail.com>
2022-02-12 18:20:06 -0500

    Styling for CardList and eventual CardDetail

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,14 +1,27 @@
 import React from "react";
 
 const CardList = ({ cards }) => {
+  if (cards.length === 0) {
+    return <div>Number of results: 0</div>;
+  }
   const renderedList = cards.map((card) => {
     return (
-      <div key={card.id}>
-        <img alt={card.name} src={card.card_images[0].image_url} />
+      <div className="five wide column" key={card.id}>
+        <img alt={card.name} src={card.card_images[0].image_url_small} />
       </div>
     );
   });
-  return <div>{renderedList}</div>;
+  return (
+    <div>
+      <div className="ui segment">
+        Your search returned {cards.length} results:
+      </div>
+      <div className="ui grid">
+        <div className="ui eleven wide column grid">{renderedList}</div>
+        <div className="ui segment five wide column">Card Info</div>
+      </div>
+    </div>
+  );
 };
 
 export default CardList;
